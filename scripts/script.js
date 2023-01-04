a952cf42815fb1900b354595f3c09c2bbec32b55
sathish <2000sathishkumar.d@gmail.com>
2023-01-04 11:44:41 +0530

    small changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,36 @@
-'use strict'
+"use strict";
 
-// get elements
-const 
+// Select all the Dom elements
+const balanceEl = document.getElementById("total-balance");
+
+const incomeEl = document.getElementById("income");
+
+const expenseEl = document.getElementById("expense");
+
+const listEl = document.getElementById("list");
+
+const formEl = document.getElementById("form");
+
+const titleEl = document.getElementById("title");
+
+const amountEl = document.getElementById("amount");
+
+// Global variable
+let transactions = [];
+
+// Functions
+function init() {
+  listEl.innerHTML = null;
+}
+
+// Event listeners
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // value tensions
+  if (titleEl.value.trim() === "" || amountEl.value.trim() === "") {
+    alert("please add Transaction Details 🙄");
+  }
+});
+
+// Initial settings
+init();
